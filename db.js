cce1234f07d3a8db6ca1f10e9ecf38c909fba74b
shoab542 <shoab542@gmail.com>
2026-05-07 10:19:08 +0600

    ready for deployment

--- db.js
+++ db.js
@@ -5,6 +5,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl:{
+    rejectUnauthorized:false
+  }
 });
 
 module.exports = db;
